aa88e6ad01b22ab12e40eb4558337f97bb59d5ad
paosantii <65790062+paosantii@users.noreply.github.com>
2022-12-12 20:09:23 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let ciudad;
+ciudad = "Baradero";
+let numero;
+numero = 32;
+console.log(ciudad);
+console.log(numero);
+
+const nombre = "Paola";
+const apellido = "Santinelli";
+
+const nombreCompleto = nombre + ' ' + apellido;
+console.log(nombreCompleto);
